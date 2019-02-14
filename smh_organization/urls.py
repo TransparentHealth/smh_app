08d47c5a38bd6eec54efd21086e5309a4beb6a6a
Dmitriy Chukhin <dchukhin@gmail.com>
2019-02-14 12:20:19 -0500

    add simple login page that redirects to home page

--- smh_organization/urls.py
+++ smh_organization/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.views.generic.base import TemplateView
-from django.urls import path
+from django.urls import include, path
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='homepage.html'), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
