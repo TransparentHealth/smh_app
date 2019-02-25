e9a95c51b4998c883aab2df9ec8c902fcfdd9b71
Dmitriy Chukhin <dchukhin@gmail.com>
2019-02-25 14:38:24 -0500

    simplify backend definition to be more like what is used in sharemyhealth repo

--- apps/vmi/backends.py
+++ apps/vmi/backends.py
@@ -1,40 +1,20 @@
 from django.conf import settings
 
-from social_core.backends.oauth import BaseOAuth2
+from social_core.backends.open_id_connect import OpenIdConnectAuth
 
 
-class VMIOAuth2Backend(BaseOAuth2):
-    """A backend for authenticating to VerifyMyIdentity (VMI)."""
+class VMIOAuth2Backend(OpenIdConnectAuth):
+    """
+    An OAuth backend, similar to what is used in sharemyhealth repo.
+
+    For reference, the sharemyhealth use is: https://github.com/TransparentHealth/
+    sharemyhealth/blob/master/apps/verifymyidentity/authentication.py.
+    """
     name = getattr(settings, 'SOCIAL_AUTH_VMI_NAME')
-    SOCIAL_AUTH_VMI_HOST = getattr(settings, 'SOCIAL_AUTH_VMI_HOST')
-    ID_KEY = 'email'
-    AUTHORIZATION_URL = SOCIAL_AUTH_VMI_HOST + '/o/authorize/'
-    ACCESS_TOKEN_URL = SOCIAL_AUTH_VMI_HOST + '/o/token/'
 
-    def get_user_profile_url(self):
-        """
-        Return the url to the user profile endpoint.
-        """
-        user_profile_url = getattr(settings, 'SOCIAL_AUTH_VMI_HOST') + '/userprofile'
-        return user_profile_url
+    # differs from value in discovery document
+    # http://openid.net/specs/openid-connect-core-1_0.html#rfc.section.15.6.2
 
-    def get_user_id(self, details, response):
-        # Extracts the user id from `user_data` response.
-        return response.get(self.ID_KEY)
-
-    def get_user_details(self, response):
-        """Return user details from response."""
-        return {
-            'username': response.get('sub'),
-            'first_name': response.get('given_name'),
-            'last_name': response.get('family_name'),
-            'email': response.get('email'),
-            'patient': response.get('patient'),
-        }
-
-    def user_data(self, access_token, *args, **kwargs):
-        """Loads user data from service."""
-        return self.get_json(
-            self.get_user_profile_url(),
-            params={'access_token': access_token}
-        )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.OIDC_ENDPOINT = getattr(settings, 'SOCIAL_AUTH_VMI_HOST')
