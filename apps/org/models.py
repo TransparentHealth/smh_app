695d3f8537104cc846fc2fcc7899ab76493c0678
Dmitriy Chukhin <dchukhin@gmail.com>
2019-03-06 10:08:11 -0500

    create an Organization model

--- apps/org/models.py
+++ apps/org/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    """An Organization."""
+    slug = models.SlugField(unique=True, max_length=255)
+    name = models.CharField(unique=True, max_length=255)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+
+    def __str__(self):
+        return self.name
