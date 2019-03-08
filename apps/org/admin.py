33d26803e35641625d2a2fdb7a0233cbdc3a7e0a
Dmitriy Chukhin <dchukhin@gmail.com>
2019-03-08 14:29:57 -0500

    show OrgResourceAccess instances in admin

--- apps/org/admin.py
+++ apps/org/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Organization
+from .models import Organization, OrgResourceAccess
 
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     search_fields = ('name',)
+
+
+@admin.register(OrgResourceAccess)
+class OrgResourceAccessAdmin(admin.ModelAdmin):
+    search_fields = ('organization__name', 'user__username')
