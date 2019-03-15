4fe9310051ca88918c38ee4828db991c4a7c2b85
Dmitriy Chukhin <dchukhin@gmail.com>
2019-03-15 12:32:22 -0400

    Resource class defines its name, rather than relying on name to be passed to its methods

--- apps/sharemyhealth/resources.py
+++ apps/sharemyhealth/resources.py
@@ -4,6 +4,8 @@ from social_django.models import UserSocialAuth
 class Resource(object):
     """A python wrapper around the social_django.models.UserSocialAuth class."""
     model_class = UserSocialAuth
+    # The name matches the model_class's 'provider' field value
+    name = 'sharemyhealth'
 
-    def filter_by_user_and_provider(self, user, provider):
-        return self.model_class.objects.filter(user=user, provider=provider)
+    def filter_by_user(self, user):
+        return self.model_class.objects.filter(user=user, provider=self.name)
