0359b01bda709ef4a9673ddd39883f6eded1eb3d
Dmitriy Chukhin <dchukhin@gmail.com>
2019-03-28 16:50:18 -0400

    records view uses get_member_data() utility function to get member data

--- apps/member/tests/test_views.py
+++ apps/member/tests/test_views.py
@@ -253,6 +253,102 @@ class RevokeResourceRequestTestCase(SMHAppTestMixin, TestCase):
         self.assertIsNone(getattr(self.resource_request, 'resourcegrant', None))
 
 
+class RecordsViewTestCase(SMHAppTestMixin, TestCase):
+    url_name = 'member:records'
+
+    def setUp(self):
+        super().setUp()
+        # When we mock uses of the requests library in this test class, this is
+        # the expected mock response.
+        self.expected_response_success = {
+            'status_code': 200,
+            'content': {'test_key': 'Test content'}
+        }
+
+    @all_requests
+    def response_content_success(self, url, request):
+        return self.expected_response_success
+
+    def give_self_user_access_to_member_token(self, member, provider_name):
+        """
+        Give the self.user access to the member's access_token, and return access_token.
+
+        This method creates the necessary database objects so that the self.user
+        is in an Organization that has a ResourceGrant for the member's Resource,
+        so the self.user can use the member's access_token to get data about the
+        member.
+        """
+        # The self.user is a part of an Organization
+        organization = OrganizationFactory()
+        organization.users.add(self.user)
+        # The member has received an access_token to get their own data.
+        provider_name = provider_name
+        access_token = 'accessTOKENhere'
+        UserSocialAuthFactory(
+            user=member,
+            provider=provider_name,
+            extra_data={'refresh_token': 'refreshTOKEN', 'access_token': access_token}
+        )
+        # The member has approved the Organization's request for the member's data
+        resource_request = ResourceRequestFactory(
+            member=member,
+            organization=organization,
+            resourcegrant=None,
+            status=REQUEST_APPROVED
+        )
+        ResourceGrantFactory(
+            member=resource_request.member,
+            organization=resource_request.organization,
+            resource_class_path=resource_request.resource_class_path,
+            resource_request=resource_request
+        )
+
+    def test_context_data(self):
+        """GETting records view puts response of get_member_data() into the context."""
+        # Create a member
+        member = UserFactory()
+        # Give the self.user access to the member's access_token.
+        provider_name = Resource.name
+        self.give_self_user_access_to_member_token(member, provider_name)
+
+        url = reverse(self.url_name, kwargs={'pk': member.pk})
+        # We mock the use of the requests library, so we don't make real
+        # requests from within the test.
+        with HTTMock(self.response_content_success):
+            response = self.client.get(url)
+
+        # The response has data,
+        # which matches the mocked response.
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.context_data.get('data').json(),
+            self.expected_response_success['content']
+        )
+
+    def test_authenticated(self):
+        """The user must be authenticated to see records."""
+        # Create a member
+        member = UserFactory()
+        # Give the self.user access to the member's access_token.
+        provider_name = Resource.name
+        self.give_self_user_access_to_member_token(member, provider_name)
+        url = reverse(self.url_name, kwargs={'pk': member.pk})
+
+        with self.subTest('Authenticated'):
+            self.client.force_login(self.user)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+
+        with self.subTest('Not authenticated'):
+            self.client.logout()
+            response = self.client.get(url)
+            expected_redirect = '{}?next={}'.format(
+                reverse('login'),
+                url
+            )
+            self.assertRedirects(response, expected_redirect)
+
+
 class DataSourcesViewTestCase(SMHAppTestMixin, TestCase):
     url_name = 'member:data-sources'
 
