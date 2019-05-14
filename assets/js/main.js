475b5452fe88f710b49583b5a088288bb4d9997f
philtuskha <phil.tuskha@gmail.com>
2019-05-14 12:52:17 -0400

    create member modal (#56)
    * create member modal
    * removed style ref
    * temporary email is made up of only valid characters (#57)
    * create member all views
    * remove <<<<<<
    * added member copy option styling, etc
    * check for `.first` instead of organization_set
    * flake8

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
 const member = require('./member/main')
 
-const editMember = document.getElementById('edit_member')
-if (editMember) {
-  // if member template is loaded add listener to edit_member element
-  editMember.onclick = member.toggleEditMember
-}
+document.addEventListener("DOMContentLoaded", function() {
+
+	const editMember = document.getElementById('edit_member')
+	if (editMember) {
+	  // if member template is loaded add listener to edit_member element
+	  editMember.onclick = member.toggleEditMember
+	}
+
+	// open and close modal
+	const memberModal = document.getElementById('create-member-modal')
+	const createAccountBtn = document.getElementById('create-account-btn')
+	const backdrop = document.getElementById('create-member-modal-backdrop')
+	function openMemberModal() {
+		memberModal.classList.add('show-modal')
+	}
+	function closeMemberModal() {
+		memberModal.classList.remove('show-modal')
+	}
+	createAccountBtn.onclick = openMemberModal
+	backdrop.onclick = closeMemberModal
+
+})
