e68678185b797a05eab6da637e0a5b126b6f9e46
whytheplatypus <whytheplatypus@gmail.com>
2019-05-17 18:21:55 -0400

    Load aws param store env vars for manage commands

--- manage.py
+++ manage.py
@@ -3,12 +3,24 @@ import os
 import sys
 
 from smh_app import load_env
+from smh_app.ssmenv import EC2ParameterStore
 
 
 load_env.load_env()
 
 
 if __name__ == '__main__':
+
+    try:
+        parameter_store = EC2ParameterStore(region_name="us-east-1")
+        # Automate env (dev)
+        django_parameters = parameter_store.get_parameters_by_path(
+            '/dev/smhapp/',
+            strip_path=True)
+        EC2ParameterStore.set_env(django_parameters)
+    except Exception:
+        pass
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smh_app.settings')
     try:
         from django.core.management import execute_from_command_line
