1e7aa3c8e38467fbb5521c0555a7b942c9cd997a
Sean Harrison <sah@bookgenesis.com>
2019-06-06 19:20:44 -0500

    add SocialAuthExceptionMiddleware

--- smh_app/settings.py
+++ smh_app/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'smh_app.urls'
