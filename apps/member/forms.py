ef4d30e211c4fee5e90c170749ba40e88806c5cc
Sean Harrison <sah@bookgenesis.com>
2019-06-17 17:48:51 -0500

    Build member organizations page, with modals to approve / deny / revoke organizational access to member data

---
+++ apps/member/forms.py
@@ -0,0 +1,9 @@
+
+from django import forms
+from apps.org.models import ResourceRequest
+
+class ResourceRequestForm(forms.ModelForm):
+    class Meta:
+        model = ResourceRequest
+        fields = '__all__'
+
