b8319653a1a1625a13cadc5ffb5c0a6e7740a4be
Sean Harrison <40370408+sharrisoncaktus@users.noreply.github.com>
2019-07-05 13:09:07 -0500

    Medical data update (#140)
    * Remove dupllicate get_id_token_payload
    
    * Update settings.MEMBER_DATA_RECORD_TYPE_MAPPING
    
    * Remove profile picture if not in id_token payload
    
    * formatting
    
    * Build prescriptions data, show prescriptions in UI.
    
    * object-fit user icon on org dashboard
    
    * Finish views for records/prescriptions and records/procedures.
    Remove unused code (get_member_data) and tests.

--- apps/member/utils.py
+++ apps/member/utils.py
@@ -1,97 +1,82 @@
-from collections import defaultdict
-from importlib import import_module
-import json
+import hashlib
+import base64
+from datetime import datetime
 
 import requests
 from django.conf import settings
-from django.shortcuts import Http404
 from jwkest.jwt import JWT
-from apps.org.models import ResourceGrant
 
 
-def get_member_data(requesting_user, member, resource_name, record_type):
-    """Get the data for a member from a resource."""
+def get_prescriptions(data):
+    """Returns an id-keyed dict of all prescriptions in the (FHIR) medical data
+    * Medication = reference field to join medication resources
+        * .id = unique id in this data set
+        * .code.text = display name
+    * MedicationRequest = records a doctor's request for a medication
+        * .medicationReference
+            * .reference = "Medication/{id}" that this request is for
+            * .display = Medication's display name
+        * .requester.agent
+            * .reference = "Practioner/{id}" points to a Physician
+            * .display = Physician's display name
+    * MedicationDispense = records the dispensation of a medication
+        * .status = the status of the dispensation ("completed") 
+        * .medicationReference{.reference, .display}
+    * MedicationStatement = records the prescription itself
+        * .status
+        * .effectivePeriod{.start, .end}
+        * .taken
+        * .dosage[0].doseQuantity{.value, .unit}
 
-    # Get the path to the resource class from the settings, based on the
-    # resource_name.
-    resource_class_path = settings.RESOURCE_NAME_AND_CLASS_MAPPING.get(
-        resource_name, None)
-    # If there is not a path for the resource_name, raise an error
-    if not resource_class_path:
-        raise Http404
+    Combine records that have the same name, when the date is the same as the previous one or null
+    """
+    prescriptions = {}
+    prev_date = None
+    prev_name = None
+    for entry in data['entry']:
+        resource = entry['resource']
+        if resource['resourceType'] in [
+                'MedicationRequest',
+                'MedicationStatement',
+                'MedicationDispense',
+        ]:
+            name = resource['medicationReference']['display']
+            date = resource.get('effectivePeriod', {}).get('start', None)
 
-    # Is the record_type is not valid, raise an error
-    if record_type not in settings.VALID_MEMBER_DATA_RECORD_TYPES:
-        raise Http404
+            # Combine records that have the same name, when the date is the same or null
+            if (name and name == prev_name
+                    and ((date and (date == prev_date)) or (not date and prev_date))):
+                date = prev_date
 
-    # Users are allowed to request their own data. If the requesting_user is not
-    # the member, then check if the requesting_user's Organization has access
-    # to this member's resource.
-    if requesting_user == member.user:
-        resource_module = '.'.join(resource_class_path.split('.')[:-1])
-        resource_class_name = resource_class_path.split('.')[-1]
-        resource_class = getattr(import_module(
-            resource_module), resource_class_name)
+            prev_date = date
+            prev_name = name
 
-        response = resource_class(member.user).get(record_type)
-    else:
-        resource_grants = ResourceGrant.objects.filter(
-            member=member.user,
-            resource_class_path=resource_class_path,
-            organization__users=requesting_user
-        )
-        if not resource_grants.exists():
-            raise Http404
-        else:
-            resource_grant = resource_grants.first()
-        response = resource_grant.resource_class(
-            resource_grant.member).get(record_type)
+            # build the id from the date and name
+            id_hash = hashlib.new('sha256')
+            id_hash.update((name + date).encode('utf-8'))
+            id = base64.urlsafe_b64encode(id_hash.digest())
 
-    # Loop through the content of the response, and put the fields that will need
-    # to be shown in the template into a dictionary, where the keys are record
-    # types.
-    records_dict = defaultdict(list)
-    for record in json.loads(response.content)['entry']:
-        # These are the data that will be needed for the template
-        record_data = {
-            'date': record['resource']['assertedDate'],
-            'code': record['resource']['code']['coding'][0]['code'],
-            'diagnosis': record['resource']['code']['text'],
-            'provider': '',
-        }
-        # Add the data into the records_dict
-        records_dict[record['resource']['resourceType']].append(record_data)
+            prescriptions.setdefault(id, {'name': resource['medicationReference']['display']})
 
-    # Loop through the valid record types, and create a list of dictionaries,
-    # where the keys are the record types, and the values are the data that will
-    # be shown in the template for that record type. Note: the results list can
-    # include record types that have no data, in which case just their 'name'
-    # and 'icon_src' are returned, so those can still be rendered in the
-    # template.
-    results = []
-    for valid_record_type in settings.VALID_MEMBER_DATA_RECORD_TYPES:
-        # Find the record type name that will be used in the template
-        if valid_record_type in settings.MEMBER_DATA_RECORD_TYPE_MAPPING.keys():
-            record_type_name = settings.MEMBER_DATA_RECORD_TYPE_MAPPING[
-                valid_record_type]
-        else:
-            record_type_name = valid_record_type
+            if resource['resourceType'] == "MedicationRequest":
+                prescriptions[id]['agent'] = resource.get('requester', {}).get('agent', {})
+                prescriptions[id]['request_status'] = resource['status']
+            elif resource['resourceType'] == "MedicationDispense":
+                prescriptions[id]['dispense_status'] = resource['status']
+            elif resource['resourceType'] == "MedicationStatement":
+                prescriptions[id]['effectivePeriod'] = {
+                    key: datetime.strptime(value, '%Y-%m-%dT%H:%M:%S%z')  # ISO-formatted
+                    for key, value in resource['effectivePeriod'].items()
+                }
+                prescriptions[id]['dosage'] = {
+                    k: v
+                    for k, v in resource['dosage'][0]['doseQuantity'].items()
+                    if k in ['value', 'unit']
+                }
+                prescriptions[id]['taken'] = resource['taken']
+                prescriptions[id]['statement_status'] = resource['status']
 
-        # Create a dictionary of data for this record type
-        record_type_data = {
-            'name': record_type_name.replace('_', ' ').title(),
-            'icon_src': '/images/icons/{}.png'.format(record_type_name.replace('_', '-')),
-        }
-        if valid_record_type in records_dict:
-            record_type_data['records'] = records_dict[valid_record_type]
-        else:
-            record_type_data['records'] = []
-        record_type_data['total'] = len(record_type_data['records'])
-
-        # Add the dictionary of data for this record type to the results
-        results.append(record_type_data)
-
-    return results
+    return prescriptions
 
 
 def fetch_member_data(member, provider):
