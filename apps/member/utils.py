0dc50f45017256dafb49358c8eef8185aa821f09
Sean Harrison <40370408+sharrisoncaktus@users.noreply.github.com>
2019-07-10 11:50:52 -0500

    Reintroduce memoize cache (#144)
    * Fallback to None if member.utils.fetch_member_data() returns nothing
    
    * Re-introduce member data memoization, and delete that cache if no data is returned.

--- apps/member/utils.py
+++ apps/member/utils.py
@@ -3,6 +3,7 @@ import base64
 from datetime import datetime
 import dateparser
 
+from memoize import memoize
 import requests
 from django.conf import settings
 from jwkest.jwt import JWT
@@ -88,9 +89,12 @@ def get_prescriptions(data):
     return prescriptions
 
 
+@memoize(timeout=300)
 def fetch_member_data(member, provider):
-    '''Fetch FHIR data from HIXNY data provider (sharemyhealth)'''
-    url = f"{settings.SOCIAL_AUTH_SHAREMYHEALTH_HOST}/hixny/api/fhir/stu3/Patient/$everything?refresh=true"
+    '''Fetch FHIR data from HIXNY data provider (sharemyhealth)
+    If data not available, returns None
+    '''
+    url = f"{settings.SOCIAL_AUTH_SHAREMYHEALTH_HOST}/hixny/api/fhir/stu3/Patient/$everything"
     sa = member.user.social_auth.filter(provider=provider).first()
     if sa is not None:
         access_token = sa.extra_data.get('access_token')
@@ -98,8 +102,6 @@ def fetch_member_data(member, provider):
             r = requests.get(url, headers={'Authorization': 'Bearer %s' % access_token})
             if r.status_code == 200:
                 return r.json()
-    # fallback: empty member data
-    return {'entry': []}
 
 
 def get_resource_data(data, resource_type):
