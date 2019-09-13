ce3b4204a63c9bea46d10327ca12f51ec1bb52ba
Sean Harrison <sah@bookgenesis.com>
2019-09-13 10:55:13 -0500

    + Encounter data model + Practitioner.names – string of names + Practitioner.next_encounter(encounters) for a given list of encounters

---
+++ apps/data/models/encounter.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass, field
+from typing import List
+from .model import DataModel
+from .types import CodeableConcept, Identifier, Period, Reference
+
+
+@dataclass
+class EncounterParticipant(DataModel):
+    type: List[CodeableConcept] = field(default_factory=list)
+    period: Period = field(default_factory=Period)
+    individual: Reference = field(default_factory=Reference)
+
+    CONVERTERS = {
+        'type': [lambda value: [CodeableConcept.from_data(val) for val in value]],
+        'period': [Period.from_data],
+        'individual': [Reference.from_data],
+    }
+
+
+@dataclass
+class EncounterLocation(DataModel):
+    location: Reference = field(default_factory=Reference)
+
+    CONVERTERS = {'location': [Reference.from_data]}
+
+
+@dataclass
+class Encounter(DataModel):
+    resourceType = 'Encounter'
+
+    id: str = field()
+    status: str = field()  # planned | arrived | triaged | in-progress | onleave | finished | cancelled | entered-in-error | unknown
+
+    identifier: List[Identifier] = field(default_factory=list)
+    type: List[CodeableConcept] = field(default_factory=list)
+    subject: Reference = field(default_factory=Reference)
+    participant: List[EncounterParticipant] = field(default_factory=list)
+    period: Period = field(default=None)
+    location: List[EncounterLocation] = field(default_factory=list)
+
+    CONVERTERS = {
+        'identifier': [lambda value: [Identifier.from_data(val) for val in value]],
+        'type': [lambda value: [CodeableConcept.from_data(val) for val in value]],
+        'subject': [Reference.from_data],
+        'participant': [
+            lambda value: [EncounterParticipant.from_data(val) for val in value]
+        ],
+        'period': [Period.from_data],
+        'location': [lambda value: [EncounterLocation.from_data(val) for val in value]],
+    }
+    VALIDATORS = {}
+
+    @property
+    def locations(self):
+        return '; '.join(l.location.display for l in self.location)
+    
