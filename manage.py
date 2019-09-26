f9d0c7672a191a7329f42a44487ed1088632c3e8
Alan Viars <aviars@videntity.com>
2019-09-26 09:54:55 -0400

    Parameterize manage.py and wsgi.py

--- manage.py
+++ manage.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python
 import os
 import sys
-
 from smh_app.ssmenv import EC2ParameterStore
 import dotenv
+from getenv import env
+
+AWS_DEFAULT_REGION = env('AWS_DEFAULT_REGION', 'us-east-1')
+VPC_ENV = env('VPC_ENV', 'dev')
+VPC_APP_NAME = env('VPC_APP_NAME', 'smhapp')
+PARAMETER_STORE_PATH = "/%s/%s/" % (VPC_ENV, VPC_APP_NAME)
+ENVIRONMENT_VARIABLE_STRATEGY = env('ENVIRONMENT_VARIABLE_STRATEGY', '.ENV')
 
 if __name__ == '__main__':
     # Get this from .env file (controlled by Ansible)
-    dotenv.load_dotenv()
-    # EC2 Parameter Store TODO Better parameterization here.
+    if ENVIRONMENT_VARIABLE_STRATEGY == '.ENV':
+        dotenv.load_dotenv()
+    # EC2 Parameter Store TODO Better parametrization here.
     try:
-        parameter_store = EC2ParameterStore(region_name="us-east-1")
-        # Automate env (dev)
-        django_parameters = parameter_store.get_parameters_by_path(
-            '/dev/smhapp/', strip_path=True
-        )
-        EC2ParameterStore.set_env(django_parameters)
+        if ENVIRONMENT_VARIABLE_STRATEGY == 'EC2_PARAMSTORE':
+            parameter_store = EC2ParameterStore(region_name=AWS_DEFAULT_REGION)
+            django_parameters = parameter_store.get_parameters_by_path(
+                PARAMETER_STORE_PATH, strip_path=True)
+            EC2ParameterStore.set_env(django_parameters)
+
     except Exception:
         pass
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smh_app.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
--- smh_app/wsgi.py
+++ smh_app/wsgi.py
@@ -8,18 +8,29 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+import dotenv
+from getenv import env
 from django.core.wsgi import get_wsgi_application
-
 from .ssmenv import EC2ParameterStore
 
+AWS_DEFAULT_REGION = env('AWS_DEFAULT_REGION', 'us-east-1')
+VPC_ENV = env('VPC_ENV', 'dev')
+VPC_APP_NAME = env('VPC_APP_NAME', 'smhapp')
+PARAMETER_STORE_PATH = "/%s/%s/" % (VPC_ENV, VPC_APP_NAME)
+ENVIRONMENT_VARIABLE_STRATEGY = env('ENVIRONMENT_VARIABLE_STRATEGY', '.ENV')
+
 try:
-    parameter_store = EC2ParameterStore(region_name="us-east-1")
-    # Automate env (dev)
-    django_parameters = parameter_store.get_parameters_by_path(
-        '/dev/smhapp/', strip_path=True
-    )
-    EC2ParameterStore.set_env(django_parameters)
+    # Get this from .env file (controlled by Ansible)
+    if ENVIRONMENT_VARIABLE_STRATEGY == '.ENV':
+        dotenv.load_dotenv()
+    # Get Via EC2 PArameter store
+    if ENVIRONMENT_VARIABLE_STRATEGY == 'EC2_PARAMSTORE':
+        parameter_store = EC2ParameterStore(region_name=AWS_DEFAULT_REGION)
+        # Automate env (dev)
+        django_parameters = parameter_store.get_parameters_by_path(
+            PARAMETER_STORE_PATH, strip_path=True
+        )
+        EC2ParameterStore.set_env(django_parameters)
 except Exception:
     pass
 
