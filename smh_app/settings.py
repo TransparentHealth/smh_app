fedc897bd1b7f67164a77b3ad4301ed3f898c95e
Alan Viars <aviars@videntity.com>
2019-09-26 12:14:53 -0400

    Fix env params

--- smh_app/settings.py
+++ smh_app/settings.py
@@ -179,7 +179,7 @@ AUTHENTICATION_BACKENDS = (
 # When a user logs in, they are redirected to the appropriate page by the
 # user_router
 LOGIN_REDIRECT_URL = 'users:user_router'
-LOGIN_URL = '/social-auth/login/vmi'
+LOGIN_URL = '/social-auth/login/verifymyidentity-openidconnect'
 
 # Settings for social_django
 SOCIAL_AUTH_URL_NAMESPACE = "social"
--- smh_app/wsgi.py
+++ smh_app/wsgi.py
@@ -17,22 +17,26 @@ AWS_DEFAULT_REGION = env('AWS_DEFAULT_REGION', 'us-east-1')
 VPC_ENV = env('VPC_ENV', 'dev')
 VPC_APP_NAME = env('VPC_APP_NAME', 'smhapp')
 PARAMETER_STORE_PATH = "/%s/%s/" % (VPC_ENV, VPC_APP_NAME)
-ENVIRONMENT_VARIABLE_STRATEGY = env('ENVIRONMENT_VARIABLE_STRATEGY', '.ENV')
+
 
 try:
+    ENVIRONMENT_VARIABLE_STRATEGY = env('ENVIRONMENT_VARIABLE_STRATEGY', '.ENV')
     # Get this from .env file (controlled by Ansible)
     if ENVIRONMENT_VARIABLE_STRATEGY == '.ENV':
+        print('EVS is', ENVIRONMENT_VARIABLE_STRATEGY)
         dotenv.load_dotenv()
-    # Get Via EC2 PArameter store
+    # Get via EC2 Parameter store
     if ENVIRONMENT_VARIABLE_STRATEGY == 'EC2_PARAMSTORE':
+        print('EVS is', ENVIRONMENT_VARIABLE_STRATEGY)
         parameter_store = EC2ParameterStore(region_name=AWS_DEFAULT_REGION)
         # Automate env (dev)
         django_parameters = parameter_store.get_parameters_by_path(
             PARAMETER_STORE_PATH, strip_path=True
         )
         EC2ParameterStore.set_env(django_parameters)
-except Exception:
-    pass
+
+except Exception as e:
+    print("Exception", e)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smh_app.settings')
 
