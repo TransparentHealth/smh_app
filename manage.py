c687312767ccae14d84ee25e68b6358025f644a5
Alan Viars <aviars@videntity.com>
2019-09-30 14:54:46 -0700

    Making parameter logic match sister projects.

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-from smh_app.ssmenv import EC2ParameterStore
+from sharemyhealth.ssmenv import EC2ParameterStore
 import dotenv
 from getenv import env
+from sharemyhealth.settings import EC2PARAMSTORE_4_ENVIRONMENT_VARIABLES
+
 
 AWS_DEFAULT_REGION = env('AWS_DEFAULT_REGION', 'us-east-1')
 VPC_ENV = env('VPC_ENV', 'dev')
@@ -12,24 +14,24 @@ PARAMETER_STORE_PATH = "/%s/%s/" % (VPC_ENV, VPC_APP_NAME)
 
 
 if __name__ == '__main__':
+
+    # Defaulting to .env
+    # Get this from .env file from your local development environment or
+    # Ansible,
     dotenv.load_dotenv()
-    ENVIRONMENT_VARIABLE_STRATEGY = env(
-        'ENVIRONMENT_VARIABLE_STRATEGY', 'EC2_PARAMSTORE')
-    # Get this from .env file (controlled by Ansible)
-    if ENVIRONMENT_VARIABLE_STRATEGY == '.ENV':
-        print('EVS is', ENVIRONMENT_VARIABLE_STRATEGY)
-    # EC2 Parameter Store TODO Better parametrization here.
+    ENVIRONMENT_VARIABLE_STRATEGY = ".ENV"
+    # EC2 Parameter Store
     try:
-        if ENVIRONMENT_VARIABLE_STRATEGY == 'EC2_PARAMSTORE':
-            print('EVS is', ENVIRONMENT_VARIABLE_STRATEGY)
+        if EC2PARAMSTORE_4_ENVIRONMENT_VARIABLES == "EC2_PARAMSTORE":
             parameter_store = EC2ParameterStore(region_name=AWS_DEFAULT_REGION)
             django_parameters = parameter_store.get_parameters_by_path(
                 PARAMETER_STORE_PATH, strip_path=True)
             EC2ParameterStore.set_env(django_parameters)
-
+            ENVIRONMENT_VARIABLE_STRATEGY = EC2PARAMSTORE_4_ENVIRONMENT_VARIABLES
     except Exception as e:
         print("Exception", e)
-
+    print('ENVIRONMENT_VARIABLE_STRATEGY in manage.py is',
+          ENVIRONMENT_VARIABLE_STRATEGY)
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smh_app.settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
