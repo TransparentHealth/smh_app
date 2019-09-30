4fe8a1dd46a24a7d4ed2f0a0c4cb6c9d0515ee25
Alan Viars <aviars@videntity.com>
2019-09-30 18:14:37 -0400

    Params (#230)
    * ensure images are updated each time
    
    * improve the login page look
    
    * Display the picture URL.
    
    * improve login screen look to be centered
    
    * Add log to logout
    
    * Making parameter logic match sister projects.
    
    * fix ref
    
    * fix ref

--- smh_app/wsgi.py
+++ smh_app/wsgi.py
@@ -20,24 +20,26 @@ PARAMETER_STORE_PATH = "/%s/%s/" % (VPC_ENV, VPC_APP_NAME)
 
 
 try:
+    # Defaulting to .env
+    # Get this from .env file from your local development environment or
+    # Ansible,
+
     dotenv.load_dotenv()
-    ENVIRONMENT_VARIABLE_STRATEGY = env('ENVIRONMENT_VARIABLE_STRATEGY', 'EC2_PARAMSTORE')
-    # Get this from .env file (controlled by Ansible)
-    if ENVIRONMENT_VARIABLE_STRATEGY == '.ENV':
-        print('EVS is', ENVIRONMENT_VARIABLE_STRATEGY)
+    ENVIRONMENT_VARIABLE_STRATEGY = '.ENV'
+    EC2PARAMSTORE_4_ENVIRONMENT_VARIABLES = env('EC2PARAMSTORE_4_ENVIRONMENT_VARIABLES',
+                                                'EC2_PARAMSTORE')
     # Get via EC2 Parameter store
-    if ENVIRONMENT_VARIABLE_STRATEGY == 'EC2_PARAMSTORE':
-        print('EVS is', ENVIRONMENT_VARIABLE_STRATEGY)
+    if EC2PARAMSTORE_4_ENVIRONMENT_VARIABLES == "EC2_PARAMSTORE":
         parameter_store = EC2ParameterStore(region_name=AWS_DEFAULT_REGION)
-        # Automate env (dev)
         django_parameters = parameter_store.get_parameters_by_path(
             PARAMETER_STORE_PATH, strip_path=True
         )
         EC2ParameterStore.set_env(django_parameters)
-
+        ENVIRONMENT_VARIABLE_STRATEGY = EC2PARAMSTORE_4_ENVIRONMENT_VARIABLES
 except Exception as e:
     print("Exception", e)
 
+print('ENVIRONMENT_VARIABLE_STRATEGY in wsgi.py is',
+      ENVIRONMENT_VARIABLE_STRATEGY)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smh_app.settings')
-
 application = get_wsgi_application()
