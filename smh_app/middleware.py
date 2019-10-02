3e364d755a66f3b2ff5adbd6a289a307fdc52420
Sean Harrison <sah@bookgenesis.com>
2019-10-02 11:53:09 -0500

    Redirect to home if AuthCanceled. This works both for auth to VMI (not yet logged in) and for authorizing SMH (home redirects to dashboard, nevermind).

---
+++ smh_app/middleware.py
@@ -0,0 +1,14 @@
+
+from social_django.middleware import SocialAuthExceptionMiddleware
+from django.shortcuts import redirect
+from django.urls import reverse
+from social_core.exceptions import AuthCanceled
+
+class AuthCanceledMiddleware(SocialAuthExceptionMiddleware):
+    def process_exception(self, request, exception):
+        """If AuthCanceled, redirect to home"""
+        if isinstance(exception, AuthCanceled):
+            return redirect(reverse('home'))
+        else:
+            return super().process_exception(request, exception)
+
