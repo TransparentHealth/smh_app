d220dd91c9cd954265732b598d24e66747b78d9c
Sean Harrison <sah@bookgenesis.com>
2019-10-02 11:54:54 -0500

    f48

--- smh_app/middleware.py
+++ smh_app/middleware.py
@@ -1,8 +1,8 @@
-
 from social_django.middleware import SocialAuthExceptionMiddleware
 from django.shortcuts import redirect
 from django.urls import reverse
 from social_core.exceptions import AuthCanceled
+
 
 class AuthCanceledMiddleware(SocialAuthExceptionMiddleware):
     def process_exception(self, request, exception):
@@ -11,4 +11,3 @@ class AuthCanceledMiddleware(SocialAuthExceptionMiddleware):
             return redirect(reverse('home'))
         else:
             return super().process_exception(request, exception)
-
