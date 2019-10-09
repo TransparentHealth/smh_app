cfe041d397cad45571731f66fedce0c38497fcc1
Alan Viars <aviars@videntity.com>
2019-10-09 16:22:02 -0400

    Fixing test

--- apps/users/tests/test_models.py
+++ apps/users/tests/test_models.py
@@ -2,9 +2,8 @@ from django.test import TestCase
 
 from apps.common.tests.factories import UserFactory
 
-
 class UserProfileTestCase(TestCase):
     def test_str(self):
         """Test for string representation."""
         user = UserFactory()
-        self.assertEqual(str(user.profile), user.username)
+        self.assertEqual(str(user.username), user.username)
