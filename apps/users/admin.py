5386720395522abf39fe0bc6d7e3479925095e63
Alan Viars <aviars@videntity.com>
2019-10-09 15:54:20 -0400

    Make sub the username.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -5,5 +5,6 @@ from .models import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user')
-    search_fields = ('user__username',)
+    list_display = ('user', 'subject', 'name')
+    search_fields = ('user__username','user__fisrt_name', 'user__last_name',
+                     'user__email') 
