7de6d86276773b3d5119dae3a89e98d2420337c4
Matthew Mosier <Matthew.Mosier@newwave.io>
2020-01-27 15:36:32 -0500

    5

--- apps/member/views.py
+++ apps/member/views.py
@@ -96,7 +96,8 @@ class SummaryView(LoginRequiredMixin, SelfOrApprovedOrgMixin, TemplateView):
         context['updated_at'] = parse_timestamp(data.get('updated_at'))
         if context['updated_at']:
             context['time_since_update'] = (
-                datetime.now(timezone.utc) - context['updated_at']
+                #datetime.now(timezone.utc) - context['updated_at']
+                context['updated_at']
             )
         fhir_data = data.get('fhir_data')
         if settings.DEBUG:
