dea5701ac692e4ecbf58fee636b7c2de16a0ba05
Alan Viars <aviars@videntity.com>
2020-05-05 15:59:36 -0400

    Code cleanup and commenting code.

--- apps/sharemyhealth/backends/sharemyhealth.py
+++ apps/sharemyhealth/backends/sharemyhealth.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from social_core.backends.open_id_connect import OpenIdConnectAuth
 
+__author__ = "Alan Viars"
+
+# Videntity Systems Inc.
+
 
 class ShareMyHealthOAuth2Backend(OpenIdConnectAuth):
     """
@@ -9,23 +13,25 @@ class ShareMyHealthOAuth2Backend(OpenIdConnectAuth):
     """
 
     name = "sharemyhealth"
+    
     # http://openid.net/specs/openid-connect-core-1_0.html#rfc.section.15.6.2
 
     DEFAULT_SCOPE = []
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.OIDC_ENDPOINT = getattr(
-            settings, 'SOCIAL_AUTH_SHAREMYHEALTH_HOST')
+        self.OIDC_ENDPOINT =  settings.SOCIAL_AUTH_SHAREMYHEALTH_HOST
         self.REVOKE_TOKEN_METHOD = "POST"  # Just set for documentation
+
+        # Note the ending slash must be present for this POST
         self.REVOKE_TOKEN_URL = "%s%s" % (
-            self.OIDC_ENDPOINT, "/o/revoke_token/")
+            settings.SOCIAL_AUTH_SHAREMYHEALTH_HOST, "/o/revoke_token/")
 
     def request_access_token(self, *args, **kwargs):
         """
         Retrieve the access token.
 
-        Since ShareMyHealth doesn't return us an id_token, we override this method
+        Since ShareMyHealth doesn't return an id_token, we override this method
         to remove the id_token validation.
         """
         return self.get_json(*args, **kwargs)
